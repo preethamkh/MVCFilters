ba883fd058c0d06ab09f4aee8c56c7345c685415
preethamkh <blighter.soul@gmail.com>
2016-12-02 16:27:51 +1100

    initial commit using filters

---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Filters.Infrastructure;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[CustomAuth(true)]
+        [Authorize(Users = "admin")]
+        public string Index()
+        {
+            return "This is the index action on the home controller";
+        }
+
+        [GoogleAuth]
+        public string List()
+        {
+            return "This is the List action on the home controller";
+        }
+    }
+}
