2b27ca7fa51ae392a7c725b3c6e625463eabd575
Preetham K H <blighter.soul@gmail.com>
2016-12-05 16:59:44 +1100

    Using filters and redirection

--- Filters/Controllers/HomeController.cs
+++ Filters/Controllers/HomeController.cs
@@ -21,5 +21,26 @@ namespace Filters.Controllers
         {
             return "This is the List action on the home controller";
         }
+
+        //[RangeException] - Using the RangeException Attribute in the "Infrastructure" folder
+        [HandleError(ExceptionType = typeof(ArgumentOutOfRangeException), View = "RangeError")]
+        public string RangeTest(int id)
+        {
+            if (id > 100)
+            {
+                return String.Format("The id value is: {0}", id);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("id", id, "");
+            }
+        }
+
+        //[CustomAction]
+        [ProfileAction]
+        public string FilterTest()
+        {
+            return "This is the FilterTest action";
+        }
     }
 }
