21095502f440fc31cb9d17895c183191d6c96de1
Preetham K H <blighter.soul@gmail.com>
2016-12-06 16:33:04 +1100

    Finishing filters - means of extending logic applied without needing to include that logic in the action method

---
+++ Filters/Infrastructure/SimpleMessageAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Infrastructure
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class SimpleMessageAttribute : FilterAttribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            //filterContext.HttpContext.Response.Write();
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
